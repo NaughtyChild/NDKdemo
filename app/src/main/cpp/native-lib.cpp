c19a4d3b3dcfbbdb74d66bd5f5e2c48e58511709
NaughtyChild <1050762704@qq.com>
2020-04-22 10:53:50 +0800

    增加cpp文件，增加gradle配置，增加伴随对象

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,7 +4,7 @@
 extern "C" JNIEXPORT jstring JNICALL
 Java_cn_naughtychild_ndkdemo_MainActivity_stringFromJNI(
         JNIEnv* env,
-        jobject /* this */) {
+        jobject   /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
